90110f25e26b93f4fc47b185ad557682ac38bfca
shiza1aas <ahmsjahan@gmail.com>
2015-04-12 11:53:56 +0530

    Adding second repository

--- hello.c
+++ hello.c
@@ -1,152 +1,152 @@
-// Want to use open MP in linux. You need to follow these steps :
+Want to use open MP in linux. You need to follow these steps :
 
-// 1. Install MPICC
-// 	sudo apt-get install lam4-dev
+1. Install MPICC
+	sudo apt-get install lam4-dev
 
-// 2. Run lamboot
-// 	lamboot ( Don't run it as root)
+2. Run lamboot
+	lamboot ( Don't run it as root)
 
-// 3. Now to compile a MPI program,
-//    mpicc hello.c
+3. Now to compile a MPI program,
+   mpicc hello.c
 
-// 4. To run it
-// 	mpirun -np 4 ./a.out
+4. To run it
+	mpirun -np 4 ./a.out
 
-// 	here 4 gives the no. of processes
+	here 4 gives the no. of processes
 
-// **** Hello World ***
+**** Hello World ***
 
-// hello.c file
-
-
-// #include <omp.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// int main (int argc, char *argv[]) 
-// {
-// 	int ierr;
-
-// 	ierr = MPI_Init(&argc,&argv);	
-// 	printf("Hello World.\n");
-// 	ierr = MPI_Finalize();
-// 	return 0;
-// }
-// ----------------------------
-//Now to compile a MPI program,
-//    mpicc hello.c
-
-//To run it
-// 	mpirun -np 4 ./a.out
-// --------------------------
-
-// #include <mpi.h>
-// #include <stdio.h>
-
-// int main(int argc,char *argv[])
-// {
-// 	int myrank, size;
-
-// 	MPI_Init(&argc,&argv);
-
-// 	MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
-// 	MPI_Comm_size(MPI_COMM_WORLD,&size);
-
-// 	printf("Hello world from %d out of %d\n",myrank,size );
-// 	MPI_Finalize();
-// 	return 0;
-// }
-
-// compile it as described above
-
-// Now I can use myrank to assign different task to different processes.
-
-// #include <stdio.h>
-// #include <mpi.h>
-
-// int main(int argc,char *argv[])
-// {
-// 	int myrank,mysize;
-
-// 	MPI_Init(&argc,&argv);
-
-// 	MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
-// 	MPI_Comm_size(MPI_COMM_WORLD,&mysize);
-
-// 	if ( myrank == 1 )
-// 	{
-// 		printf("Hello from %d process\n",myrank );
-// 	}
-// 	else
-// 	{
-// 		printf("Hello from other %d process\n",mysize - 1);
-// 	}
-// 	MPI_Finalize();
-// 	return 0;
-// }
-
-// ********Using Send or Recieve in MPI :: Communication routines****
-
-// MPI_Send(data_to_send,send_count,data_send_type,destination_id,tag,environment);
-
-// data_to_send :: Which data should be send. ( address of that data )
-// send_count :: The number of charachters to be send.
-// data_send_type :: Type of data to be sent.
-// destination_id :: the destination id 
-// tag :: identify each data.
-// environment :: generally MPI_COMM_WORLD.
+hello.c file
 
 
-// MPI_Recv (data_to_recieve,recieve_count,data_recv_type,source_id,tag,environment,status);
-// data_to_recieve :: data type to be recieved.
-// recieve_count :: no of data to be recieved.
-// data_recv_type :: data to be recived type ( MPI_INT).
-// source_id :: the source id.
-// tag :: tag to identify sending data.
-// environment :: generally MPI_COMM_WORLD.
-// &	status :: MPI_Status type data.
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main (int argc, char *argv[]) 
+{
+	int ierr;
+
+	ierr = MPI_Init(&argc,&argv);	
+	printf("Hello World.\n");
+	ierr = MPI_Finalize();
+	return 0;
+}
+----------------------------
+Now to compile a MPI program,
+   mpicc hello.c
+
+To run it
+	mpirun -np 4 ./a.out
+--------------------------
+
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc,char *argv[])
+{
+	int myrank, size;
+
+	MPI_Init(&argc,&argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+
+	printf("Hello world from %d out of %d\n",myrank,size );
+	MPI_Finalize();
+	return 0;
+}
+
+compile it as described above
+
+Now I can use myrank to assign different task to different processes.
+
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc,char *argv[])
+{
+	int myrank,mysize;
+
+	MPI_Init(&argc,&argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+	MPI_Comm_size(MPI_COMM_WORLD,&mysize);
+
+	if ( myrank == 1 )
+	{
+		printf("Hello from %d process\n",myrank );
+	}
+	else
+	{
+		printf("Hello from other %d process\n",mysize - 1);
+	}
+	MPI_Finalize();
+	return 0;
+}
+
+********Using Send or Recieve in MPI :: Communication routines****
+
+MPI_Send(data_to_send,send_count,data_send_type,destination_id,tag,environment);
+
+data_to_send :: Which data should be send. ( address of that data )
+send_count :: The number of charachters to be send.
+data_send_type :: Type of data to be sent.
+destination_id :: the destination id 
+tag :: identify each data.
+environment :: generally MPI_COMM_WORLD.
 
 
-// -------------------------------------------------------------------
+MPI_Recv (data_to_recieve,recieve_count,data_recv_type,source_id,tag,environment,status);
+data_to_recieve :: data type to be recieved.
+recieve_count :: no of data to be recieved.
+data_recv_type :: data to be recived type ( MPI_INT).
+source_id :: the source id.
+tag :: tag to identify sending data.
+environment :: generally MPI_COMM_WORLD.
+&	status :: MPI_Status type data.
 
-// #include <stdio.h>
-// #include <mpi.h>
 
-// int main(int argc , char *argv[])
-// {
-// 	int send,recieve,tag;
-// 	MPI_Status MPI_STATUS_SIZE;
-// 	int myrank;
-// 	send = 1000;
+-------------------------------------------------------------------
 
-// 	MPI_Init(&argc,&argv);
+#include <stdio.h>
+#include <mpi.h>
 
-// 	MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+int main(int argc , char *argv[])
+{
+	int send,recieve,tag;
+	MPI_Status MPI_STATUS_SIZE;
+	int myrank;
+	send = 1000;
 
-// 	if ( myrank == 1 )
-// 	{
-// 		printf("Sending data...\n");
-// 		MPI_Send(&send,1,MPI_INT,0,1,MPI_COMM_WORLD);
-// 	}
-// 	if ( myrank == 0 )
-// 	{
-// 		printf("Recieving data..\n");
-// 		MPI_Recv(&recieve,1,MPI_INT,1,1,MPI_COMM_WORLD,&MPI_STATUS_SIZE);
+	MPI_Init(&argc,&argv);
 
-// 		printf("Data recieved data is  : %d\n",recieve );
-// 	}
-	// MPI_Finalize();
-// 	return 0;
-// }
-// --------------------------------------------------------------
+	MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
 
-// Calculating sum of an array using MPI.
-// The question is : 
-// For a given array, enter the N ( size of the array ) .
-// Process 0 takes it (N) and sends it to all of the other processes.
-// All other processes revieves it and calculates the sum of all 
-// rank, rank + i * (no_of_processes ) elements
-// Now they send their sums to first process.
-// Process 0 calculates the sum.
+	if ( myrank == 1 )
+	{
+		printf("Sending data...\n");
+		MPI_Send(&send,1,MPI_INT,0,1,MPI_COMM_WORLD);
+	}
+	if ( myrank == 0 )
+	{
+		printf("Recieving data..\n");
+		MPI_Recv(&recieve,1,MPI_INT,1,1,MPI_COMM_WORLD,&MPI_STATUS_SIZE);
+
+		printf("Data recieved data is  : %d\n",recieve );
+	}
+	MPI_Finalize();
+	return 0;
+}
+--------------------------------------------------------------
+
+Calculating sum of an array using MPI.
+The question is : 
+For a given array, enter the N ( size of the array ) .
+Process 0 takes it (N) and sends it to all of the other processes.
+All other processes revieves it and calculates the sum of all 
+rank, rank + i * (no_of_processes ) elements
+Now they send their sums to first process.
+Process 0 calculates the sum.
 
 
 #include <mpi.h>
@@ -209,7 +209,7 @@ int main(int argc, char *argv[])
 		MPI_Send(&sum,1,MPI_INT,0,myrank,MPI_COMM_WORLD);
 	}
 	MPI_Finalize();
-	return 0;
+	return
 }
 
 
